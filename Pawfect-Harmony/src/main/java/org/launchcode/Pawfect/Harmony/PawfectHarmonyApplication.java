84d9a111de75636e50b072368d3f499e3b090701
Julie Pitney <125754341+physgirl7@users.noreply.github.com>
2023-07-11 14:26:23 -0500

    Second Commit: Populated the application.properties file in order to connect to mysql including username and password, etc. Also edited the build.grade to include an extension on the implementation mysql. It was implementation 'mysql:mysql-connector-java' changed to implementation 'mysql:mysql-connector-java:8.0.15' otherwise you get error messages. Removed the exclude line for @SpringBoot in PawfectHarmonyApplication.java so it will run with database, updated useraccount.html to have all the skeleton parts of the page with no Thymeleaf yet, created a UserRepository in order to access User data from database

--- Pawfect-Harmony/src/main/java/org/launchcode/Pawfect/Harmony/PawfectHarmonyApplication.java
+++ Pawfect-Harmony/src/main/java/org/launchcode/Pawfect/Harmony/PawfectHarmonyApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication (exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class PawfectHarmonyApplication {
 
 	public static void main(String[] args) {
@@ -12,3 +12,5 @@ public class PawfectHarmonyApplication {
 	}
 
 }
+
+//(exclude = {DataSourceAutoConfiguration.class })
