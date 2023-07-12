44c8488cf183ab4482fae0a06ff2a202504c2410
Julie Pitney <125754341+physgirl7@users.noreply.github.com>
2023-07-12 16:22:52 -0500

    Third Commit: Created an html file to create users so as to populate the database. This can be replaced with a create user html file made by Ethan. I updated the UserController with a way to create a new user and then redirect to the user account page so they can see all of their info. Updated getter and setter in User.java for firstName and lastName; accidentally forgot those when I created User.java

--- Pawfect-Harmony/src/main/java/org/launchcode/Pawfect/Harmony/controllers/UserController.java
+++ Pawfect-Harmony/src/main/java/org/launchcode/Pawfect/Harmony/controllers/UserController.java
@@ -1,17 +1,46 @@
 package org.launchcode.Pawfect.Harmony.controllers;
 
+import org.launchcode.Pawfect.Harmony.data.UserRepository;
 import org.launchcode.Pawfect.Harmony.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
-    @GetMapping("useraccount")
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("useraccount/{userId}")
     public String displayUserAccountPage() {
 //        model.addAttribute("user", new User());
         return "user/useraccount";
+    }
+
+    @GetMapping("create")
+    public String displayCreateUserForm(Model model) {
+        model.addAttribute(new User());
+        return "user/create";
+    }
+
+    @PostMapping("create")
+    public String processCreateUserForm(@ModelAttribute @Valid User newUser,
+                                         Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            return "user/create";
+        }
+        User saved = userRepository.save(newUser);
+
+        return "redirect:useraccount/" + saved.getId();
     }
 }
