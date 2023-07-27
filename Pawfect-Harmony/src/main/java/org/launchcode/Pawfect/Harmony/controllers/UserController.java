3399c9d913ff4976dd39633bfcf1b8307bd51e5d
EthanWillCode <Aethil@yahoo.com>
2023-07-26 19:20:15 -0500

    removed dead code in user controller

--- Pawfect-Harmony/src/main/java/org/launchcode/Pawfect/Harmony/controllers/UserController.java
+++ Pawfect-Harmony/src/main/java/org/launchcode/Pawfect/Harmony/controllers/UserController.java
@@ -30,29 +30,6 @@ public class UserController {
         }
     }
 
-//    @GetMapping("create")
-//    public String displayCreateUserForm(Model model) {
-//        model.addAttribute(new User());
-//        return "user/create";
-//    }
-//
-//    @PostMapping("create")
-//    public String processCreateUserForm(@ModelAttribute @Valid User newUser,
-//                                         Errors errors) {
-//
-//        if (errors.hasErrors()) {
-//            return "user/create";
-//        }
-//        User existingUser = userRepository.findByUsername(newUser.getUsername());
-//
-//        if (existingUser != null) {
-//            errors.rejectValue("username", "username.alreadyexists", "A user with that username already exists");
-//            return "user/create";
-//        }
-//        userRepository.save(newUser);
-//        return "redirect:/search";
-//    }
-
     @GetMapping("edit/{userId}")
     public String displayEditUserAccount(Model model, @PathVariable int userId){
         Optional optUser = userRepository.findById(userId);
