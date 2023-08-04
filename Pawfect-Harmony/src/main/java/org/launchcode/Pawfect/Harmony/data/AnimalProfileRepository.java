d694c667bc7eea2ac45c9d418839499edad764e6
Julie Pitney <125754341+physgirl7@users.noreply.github.com>
2023-08-04 15:25:07 -0500

    committing before checking out navbar branch

--- Pawfect-Harmony/src/main/java/org/launchcode/Pawfect/Harmony/data/AnimalProfileRepository.java
+++ Pawfect-Harmony/src/main/java/org/launchcode/Pawfect/Harmony/data/AnimalProfileRepository.java
@@ -1,10 +1,13 @@
 package org.launchcode.Pawfect.Harmony.data;
 
 import org.launchcode.Pawfect.Harmony.models.AnimalProfile;
+import org.launchcode.Pawfect.Harmony.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AnimalProfileRepository extends CrudRepository<AnimalProfile, Integer> {
-
+    List<AnimalProfile> findAllByUser(User user);
 }
--- Pawfect-Harmony/src/main/java/org/launchcode/Pawfect/Harmony/data/UserMeetPetRepository.java
+++ Pawfect-Harmony/src/main/java/org/launchcode/Pawfect/Harmony/data/UserMeetPetRepository.java
@@ -1,9 +1,14 @@
 package org.launchcode.Pawfect.Harmony.data;
 
+import org.launchcode.Pawfect.Harmony.models.AnimalProfile;
+import org.launchcode.Pawfect.Harmony.models.User;
 import org.launchcode.Pawfect.Harmony.models.UserMeetPet;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserMeetPetRepository extends CrudRepository<UserMeetPet, Integer> {
+    List<UserMeetPet> findAllByUser(User user);
 }
