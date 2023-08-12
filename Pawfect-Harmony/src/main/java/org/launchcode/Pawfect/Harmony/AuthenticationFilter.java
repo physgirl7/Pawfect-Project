2e32580e6b4297891aaae252ae2ac5faec5030a9
EthanWillCode <Aethil@yahoo.com>
2023-08-12 16:07:35 -0500

    Migrated pages accociated with footer to templates.footerPages

---
+++ Pawfect-Harmony/src/main/java/org/launchcode/Pawfect/Harmony/AuthenticationFilter.java
@@ -0,0 +1,42 @@
+package org.launchcode.Pawfect.Harmony;
+
+import org.launchcode.Pawfect.Harmony.controllers.AuthenticationController;
+import org.launchcode.Pawfect.Harmony.data.UserRepository;
+import org.launchcode.Pawfect.Harmony.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class AuthenticationFilter extends HandlerInterceptorAdapter {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    AuthenticationController authenticationController;
+
+    private static final List<String> whitelist = Arrays.asList("/login", "/register", "/search", "/home", "/logout", "/css");
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws IOException {
+
+        HttpSession session = request.getSession();
+        User user = authenticationController.getUserFromSession(session);
+
+        // The user is logged in
+        if (user != null) {
+            return true;
+        }
+
+        // The user is NOT logged in
+        response.sendRedirect("/login");
+        return false;
+    }
+}
