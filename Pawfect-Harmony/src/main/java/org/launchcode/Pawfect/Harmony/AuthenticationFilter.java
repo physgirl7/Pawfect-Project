bff52d0458f022e20ee480bd7612048d3eeeabbb
EthanWillCode <Aethil@yahoo.com>
2023-08-12 20:05:55 -0500

    Updated Authentication filter

--- Pawfect-Harmony/src/main/java/org/launchcode/Pawfect/Harmony/AuthenticationFilter.java
+++ Pawfect-Harmony/src/main/java/org/launchcode/Pawfect/Harmony/AuthenticationFilter.java
@@ -22,10 +22,25 @@ public class AuthenticationFilter extends HandlerInterceptorAdapter {
     AuthenticationController authenticationController;
 
     private static final List<String> whitelist = Arrays.asList("/login", "/register", "/search", "/home", "/logout", "/css");
+
+    private static boolean isWhitelisted(String path) {
+        for (String pathRoot : whitelist) {
+            if (path.startsWith(pathRoot)) {
+                return true;
+            }
+        }
+        return false;
+    }
     @Override
     public boolean preHandle(HttpServletRequest request,
                              HttpServletResponse response,
                              Object handler) throws IOException {
+
+        // Don't require sign-in for whitelisted pages
+        if (isWhitelisted(request.getRequestURI())) {
+            // returning true indicates that the request may proceed
+            return true;
+        }
 
         HttpSession session = request.getSession();
         User user = authenticationController.getUserFromSession(session);
